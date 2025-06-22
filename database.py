85baff18f5bc7112a8123ff0c26687d647173a89
Sandesh <sdhakal712@gmail.com>
2025-06-22 14:32:42 +0545

    initial commit

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlmodel import Session , create_engine , SQLModel
 database_name = "Database.db"
 database_url = f'sqlite:///{database_name}'
 
-connect_args = {'check_same_threads':False}
+connect_args = {'check_same_thread':False}
 engine = create_engine(database_url , connect_args=connect_args)
 
 def create_db():

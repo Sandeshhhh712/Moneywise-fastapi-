96bacb268fcb3f22eec0cb724b219984a3089fad
Sandesh <sdhakal712@gmail.com>
2025-06-23 12:54:15 +0545

    created transaction and category models with fk , reverse relationship with eachother and user model for relational mapping of data

--- models.py
+++ models.py
@@ -1,9 +1,16 @@
 # creating a User Model
 
-from sqlmodel import SQLModel , Field
+from sqlmodel import Relationship, SQLModel , Field
 from typing import Optional
 from pydantic import EmailStr
 from datetime import date
+from enum import Enum
+
+
+class TransactionType(str , Enum):
+    income = "income"
+    expense = "expense"
+
 
 class User(SQLModel , table = True):
     id : Optional[int] = Field(default=None , primary_key=True)
@@ -11,3 +18,38 @@ class User(SQLModel , table = True):
     email : EmailStr
     password : str
     created_at : date = Field(default_factory=date.today)
+
+    transactions : list['Transaction'] = Relationship(back_populates="user")
+    category : list['Category'] = Relationship(back_populates="user")
+
+
+class Category(SQLModel , table=True):
+    id : Optional[int] = Field(default=None , primary_key=True)
+    name : str
+
+    user_id : int = Field(foreign_key="user.id")
+    user : Optional['User'] = Relationship(back_populates="category")
+
+    transactions : list["Transaction"] = Relationship(back_populates="category")
+
+class Transaction(SQLModel , table = True):
+    id : Optional[int] = Field(default=None , primary_key=True)
+    title : str = Field(index=True)
+    amount : int
+    type : TransactionType = Field(default=TransactionType.expense)
+
+    category_id : Optional[int] = Field(default=None , foreign_key="category.id")
+    category : Optional[Category] = Relationship(back_populates="transactions")
+
+    date_added : date = Field(default_factory=date.today)
+    optional_notes : str | None = Field(default=None)
+
+    user_id : int = Field(foreign_key="user.id")
+    user : Optional["User"] = Relationship(back_populates = "transactions")
+
+Transaction.update_forward_refs() # because model has been called before it had been made
+Category.update_forward_refs()
+User.update_forward_refs()
+
+
+ 
